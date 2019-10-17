754b8d0dbbb0e57eac816ce39a35f7c1d5f82c67
Caleb Pendergraft <cackmed@gmail.com>
2019-10-17 16:53:53 -0700

    wrote out further code

--- computer/computer.js
+++ computer/computer.js
@@ -1,4 +1,22 @@
 import loadSave from '../common/load-save.js';
 import { getSave } from '../data/data.js';
+import outOfTime from '../common/outOfTime.js';
 loadSave();
 const user = getSave();
+
+if (outOfTime(user || AllWorkFinished))
+    window.location = '../results';
+
+const nav = document.getElementById('quests');
+
+for (let i = 0; i < quest.length; i++) {
+    const quest = quest[i];
+    let questDisplay = null
+    if (user.completed[quest.id]) {
+        questDisplay = finishedWork(quest);
+    }
+    else {
+        questDisplay = createWorkLink(quest);
+    }
+    nav.appendChild(questDisplay);
+}
