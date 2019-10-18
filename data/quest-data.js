70541586f4b2caae22e2c58d0b556cc6060422a7
Caleb Pendergraft <cackmed@gmail.com>
2019-10-18 09:06:10 -0700

    intial work session commit

--- data/quest-data.js
+++ data/quest-data.js
@@ -104,3 +104,4 @@ const notSureWhatToDo = {
 };
 
 
+export default quests;
---
+++ quest/quest.js
@@ -0,0 +1,4 @@
+import choiceCreation from './choices.js';
+import statisticalChanges from './statistical-changes.js';
+import { getSave, Save } from '../data/data.js';
+import 
